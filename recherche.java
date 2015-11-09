6b50a0023bf32664dc9ad662e83ceece4be04bfb
BNgoum <benjamin.ngoum@gmail.com>
2015-11-09 16:25:41 +0100

    Create recherche.java

---
+++ recherche.java
@@ -0,0 +1,38 @@
+
+public class recherche {
+
+	public static boolean rechercheDicho(int t[], int elemt)
+	{
+		int gauche=0;
+		int droite=t.length-1;
+		int milieu;
+		int j=0;
+		boolean res=false;
+		
+		for(int i=0; i<t.length-1;i++)
+		{
+			milieu=(gauche+droite)/2;
+			
+			if(t[milieu]==elemt)
+			{
+				res=true;
+			}
+			else if(t[milieu] < elemt)
+			{
+				gauche  = milieu;
+			}
+			else
+			{
+				droite = milieu;
+			}
+		}
+		
+		return res;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int t[]={1,2,3,4,5,8,9};
+		System.out.println(rechercheDicho(t,1));
+	}
+
+}
